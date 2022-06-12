70554e9dfe3ead2cd1f2f2d48cb5f7a12367a785
ttofTnT <644809641@qq.com>
2022-06-13 07:27:34 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import conversion as cv
+def run():
+    cv.adjustment.putout(cv.audioRecognition.output())
+
+
+# Press the green  button in the gutter to run the script.
+if __name__ == '__main__':
+    run()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
